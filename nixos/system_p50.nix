fbfa800d7f1e269c5ae5176d2fb15998d8abdaa3
Nixuse <nixuse@oyra.eu>
2017-05-15 17:21:17 +0200

    add system.nix for my Laptop Thinkpad P50

--- nixos/system_p50.nix
+++ nixos/system_p50.nix
@@ -1,4 +1,19 @@
 { config, lib, pkgs, ...}: with lib;
 {
-	
+	# Hostname
+	networking.hostName = "nixos-p50";
+
+	services.xserver = {
+
+		# Graphic
+		#videoDrivers = [intel nvidia];
+
+		# Notebook
+		synaptics.enable = true;
+		synaptics.twoFingerScroll = true;
+	};
+
+	 # Bootloader
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 }
