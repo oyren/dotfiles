38e78159c0331da18902f14e4f16f4af205c0cf6
Oyren <oyren@oyra.eu>
2017-09-25 18:00:08 +0200

    new monitor setup v0.1

---
+++ common/scripts/monitor-hotplug.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if xrandr | grep ' connected' | grep -q "DP2-2"; then
+	echo "DP2-2 is plugged in"
+	xrandr --output DP2-2 --primary --mode 2560x1080 --pos 0x0
+	xrandr --output eDP1 --off
+	#xrandr --output eDP1 --mode 1920x1080 --pos 2560x0
+	bspc monitor -d I II III IV V VI VII VIII IX X
+	#bspc monitor eDP1 -d VII VIII IX X
+else
+	echo "No external monitors are plugged in"
+	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0
+	bspc monitor -d I II III IV V VI VII VIII IX X
+fi
+
+#xrandr -q | grep " connected" | cut -d ' ' -f1 $connected
