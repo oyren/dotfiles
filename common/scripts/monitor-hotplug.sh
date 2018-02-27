f7fc9ab1efc0acede298dbd94e96dd3133eac83f
Oyren <m.scheuren@oyra.eu>
2018-02-27 15:41:39 +0100

    tmp

--- common/scripts/monitor-hotplug.sh
+++ common/scripts/monitor-hotplug.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-
+sleep 3
+echo `date` >> /home/user/monitor.log
 if xrandr | grep ' connected' | grep -q "DP2-2"; then
-	echo "DP2-2 is plugged in"
+	echo "DP2-2 is plugged in" >> /home/user/monitor.log
 	xrandr --output DP2-2 --primary --mode 2560x1080 --pos 0x0
 	xrandr --output eDP1 --off
 	#xrandr --output eDP1 --mode 1920x1080 --pos 2560x0
 	bspc monitor -d I II III IV V VI VII VIII IX X
 	#bspc monitor eDP1 -d VII VIII IX X
 else
-	echo "No external monitors are plugged in"
+	echo "No external monitors are plugged in" >> /home/user/monitor.log
 	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0
 	bspc monitor -d I II III IV V VI VII VIII IX X
 fi
-
-#xrandr -q | grep " connected" | cut -d ' ' -f1 $connected
+xrandr -q | grep " connected" | cut -d ' ' -f1 >> /home/user/monitor.log
