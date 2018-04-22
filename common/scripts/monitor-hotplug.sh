e649040be2407f7519f0aa22ecf36a97ceeb9315
Oyren <m.scheuren@oyra.eu>
2018-04-22 19:55:26 +0200

    new monitor hotplug setup (more dynamic)

--- common/scripts/monitor-hotplug.sh
+++ common/scripts/monitor-hotplug.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
-sleep 3
-echo `date` >> /home/user/monitor.log
-if xrandr | grep ' connected' | grep -q "DP2-2"; then
-	echo "DP2-2 is plugged in" >> /home/user/monitor.log
+for monitor in $(bspc query -M --names); do
+	monitors+=("$monitor")
+done
+#echo ${#monitors[@]} # Number of coonnected Monitors
+
+if [ ${#monitors[@]} == 1 ]; then
+	# Single Monitor Setup
+#	xrandr --output ${monitors[0]} --primary --mode 1920x1080 --pos 0x0
+	bspc monitor ${monitors[0]} -d 1 2 3 4 5 6 7 8 9 10
+elif xrandr | grep ' connected' | grep -q "DP2-2" &&  xrandr | grep ' connected' | grep -q "eDP1"; then
+	# Home Docking Setup
 	xrandr --output DP2-2 --primary --mode 2560x1080 --pos 0x0
-	xrandr --output eDP1 --off
-	#xrandr --output eDP1 --mode 1920x1080 --pos 2560x0
-	bspc monitor -d I II III IV V VI VII VIII IX X
-	#bspc monitor eDP1 -d VII VIII IX X
+	xrandr --output eDP1 --mode 1920x1080 --pos 2560x0
+	bspc monitor DP2-2 -d 1 2 3 4 5 6 7 8
+	bspc monitor eDP1 -d 9 10
 else
-	echo "No external monitors are plugged in" >> /home/user/monitor.log
-	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0
-	bspc monitor -d I II III IV V VI VII VIII IX X
+	for monitor in $(bspc query -M); do
+		bspc monitor $monitor -d 1 2 3 4 5
+	done
 fi
-xrandr -q | grep " connected" | cut -d ' ' -f1 >> /home/user/monitor.log
+
