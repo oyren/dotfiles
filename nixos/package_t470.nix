a6a6d2c437a3f06f0f75c3753eacaf4c8b3eaa53
Moritz Scheuren <m.scheuren@oyra.eu>
2018-07-26 11:46:14 +0000

    added package.nix and system.nix for Lenovo T470

---
+++ nixos/package_t470.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, lib, stable, rolling, ...}:
+with lib;
+{
+	nixpkgs.config.allowUnfree = true;
+	environment.systemPackages =
+	(with stable; [
+		# Apperence
+		arc-theme
+		arc-icon-theme
+		gnome3.gnome_themes_standard
+		gtk_engines
+		gtk-engine-murrine
+		polybar
+		tango-icon-theme
+		rofi
+		trayer
+		xorg.xrandr
+
+    # Software
+		#calibre
+		colordiff
+   # darktable
+		#emacs
+		i3lock
+		less
+#		libreoffice
+    lxappearance
+    mirage
+    mumble
+    mpv
+		neovim
+    openconnect
+    openvpn
+		pavucontrol
+    ranger
+		roxterm
+		#screenfetch
+		seafile-client
+		#shutter
+    sshuttle
+    ssvnc
+		#steam
+	#	speedcrunch
+		thunderbird
+		tig
+		tldr
+		xarchiver
+		x11vnc
+		zathura  # PDF Viewer
+
+		# System
+		arandr
+    acpi
+		bashmount
+#    breeze-icons
+		curl
+    cryptsetup
+		davfs2
+		exfat
+		fam
+		ghc
+		git
+#		jdk
+#		jre
+		shared_mime_info
+		lxmenu-data
+		networkmanagerapplet
+		ntfs3g
+		numlockx
+ #   redshift
+    pciutils
+    python
+		unrar
+		unzip
+    wget
+		xbrightness
+		xdotool
+		xorg.libXt
+		xorg.libXtst
+    cifs_utils
+
+    #System Monitoring
+		htop # task/process-manager
+    psensor # monitor temperature
+    screenfetch # system information
+    s-tui # monitor CPU temperature, frequency, power
+	  xfce.xfce4-taskmanager
+
+
+		# Writing
+		aspell
+		aspellDicts.de
+		aspellDicts.en
+    #texlive.combined.scheme-basic
+#		texlive.combined.scheme-full
+
+    # Thinkpad
+    linuxPackages.acpi_call
+	]) ++ (with rolling; [
+	discord
+  #lxappearance
+  dino
+  emacs
+	libreoffice
+  speedcrunch
+  firefox
+  samsung-unified-linux-driver
+
+	]);
+}
+
+
--- nixos/system_t470.nix
+++ nixos/system_t470.nix
@@ -3,7 +3,27 @@
 	# Hostname
 	networking.hostName = "nixos-t470";
 
+  services.thermald.enable = true; # otherwise the cpu frequency/clock is always at maximum
+
 	services.xserver = {
+    enable = true;
+		layout = "de";
+		xkbOptions = "eurosign:e";
+
+    #desktopManager.plasma5.enable = true;
+    #desktopManager.default = "plasma5";
+		windowManager.bspwm.enable = true;
+    #windowManager.bspwm.package = "pkgs.bspwm-unstable";
+		windowManager.default = "bspwm";
+		windowManager.bspwm.configFile = "/home/sky/dotfiles/common/bspwm/bspwmrc";
+		windowManager.bspwm.sxhkd.configFile= "/home/sky/dotfiles/common/bspwm/sxhkdrc";
+		desktopManager.xterm.enable = false;
+
+		displayManager.auto = {
+			enable = true;
+			user = "user";
+		};
+
 
 		# Graphic
 		videoDrivers = ["intel"];
@@ -11,15 +31,53 @@
 		# Notebook
 		synaptics.enable = true;
 		synaptics.twoFingerScroll = true;
+		multitouch.enable = true;
+	};
+
+	#System Language
+	i18n = {
+		defaultLocale = "de_DE.UTF-8";
+	};
+
+	#Time
+	time.timeZone = "Europe/Berlin";
+
+	# ZSH
+	programs.zsh = {
+		enable = true;
+		interactiveShellInit = ''
+			export EDITOR=nvim
+			# Java
+			export _JAVA_AWT_WM_NONREPARENTING=1
+		'';
+
+		shellAliases = {
+			ls="ls --color=auto";
+			l="ls -alh";
+      r="ranger";
+		};
+	};
+
+
+	# Users
+	# sky
+	users.extraUsers.sky = {
+		isNormalUser = true;
+		home = "/home/sky";
+		extraGroups = ["davfs2""wheel" "networkmanager" "vboxusers" "dialout" "adbusers" "lp" "scanner" "plugdev" "docker"];
 	};
 
 	# Hardware
 	hardware = {
+		bluetooth.enable = true;
+    bluetooth.powerOnBoot = false;
 #	    bumblebee = {
 #		      #connectDisplay = true;
 #			  enable = true;
-#		};
+#	    };
 	};
+  # Kernel
+  # boot.kernelPackages = pkgs.linuxPackages_latest; #_4_9; #_latest;
 
   # Bootloader
 	boot.loader.systemd-boot.enable = true;
